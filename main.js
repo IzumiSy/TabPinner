4b762012cfd28284277828626514a79e46f53ea5
IzumiSy <beetle-noise@gmx.com>
2014-03-11 11:49:05 +0900

    Readablity improvement by some refactorings

--- main.js
+++ main.js
@@ -8,53 +8,73 @@ var IgnoreQueries;
 var IsPinned;
 var Targetize;
 
+function LoadQueries()
+{
+	IgnoreQueries = localStorage.EmergencyBtnIgnoreQueries;
+	Targetize = localStorage.EmergencyBtnTargetize == "true" ? true : false;
+	if (IgnoreQueries != undefined && IgnoreQueries.length != 0) {
+		IgnoreQueries = IgnoreQueries.toString().split("\n");
+	}
+}
+
+function UnpinTabs(tabs)
+{
+	for (var i = 0;i < tabs.length;i++) {
+		for (var j = 0;j < PinnedIds.length;j++) {
+			if (tabs[i].id == PinnedIds[j]) {
+				chrome.tabs.update(tabs[i].id, {pinned: false});
+			}
+		}
+	}
+}
+
+function ReorderTabs(tabs)
+{
+	for (var i = 0;i < tabs.length;i++) {
+		for (var j = 0;j < PinnedIds.length;j++) {
+			if (tabs[i].id == PinnedIds[j]) {
+				chrome.tabs.move(tabs[i].id, {index: Indexes[tabs[i].id]});
+				delete Indexes[PinnedIds[j]];
+				delete PinnedIds[j];
+			}
+		}
+	}
+}
+
+function PinTabs(tabs)
+{
+	for (var i = 0;i < tabs.length;i++) {
+		if (tabs[i].pinned == false) {
+			var tabid = tabs[i].id;
+			if (IgnoreQueries != undefined)  {
+				for (var j = 0;j < IgnoreQueries.length;j++)  {
+					var r = tabs[i].url.match(new RegExp(IgnoreQueries[j]));
+					if (Targetize) {
+						// Target mode
+						if (!r) tabid = undefined;
+					} else {
+						// Ignore mode
+						if (r) tabid = undefined;
+					}
+				}
+			}
+			PinnedIds.push(String(tabid));
+			Indexes[tabid] = tabs[i].index;
+			chrome.tabs.update(tabid, {pinned: true});
+		}
+	}
+}
+
 // Extension
 chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.getAllInWindow(undefined, function(tabs) {
 		if (PinnedIds.length <= 0) {
-			IgnoreQueries = localStorage.EmergencyBtnIgnoreQueries;
-			Targetize = localStorage.EmergencyBtnTargetize == "true" ? true : false;
-			if (IgnoreQueries != undefined && IgnoreQueries.length != 0) {
-				IgnoreQueries = IgnoreQueries.toString().split("\n");
-			}
-			for (var i = 0;i < tabs.length;i++) {
-				if (tabs[i].pinned == false) {
-					var tabid = tabs[i].id;
-					if (IgnoreQueries != undefined)  {
-						for (var j = 0;j < IgnoreQueries.length;j++)  {
-							var r = tabs[i].url.match(new RegExp(IgnoreQueries[j]));
-							if (Targetize) {
-								// Target mode
-								if (!r) tabid = undefined;
-							} else {
-								// Ignore mode
-								if (r) tabid = undefined;
-							}
-						}
-					}
-					PinnedIds.push(String(tabid));
-					Indexes[tabid] = tabs[i].index;
-					chrome.tabs.update(tabid, {pinned: true});
-				}
-			}
+			LoadQueries();
+			PinTabs(tabs);
 			chrome.browserAction.setBadgeText({text: PinnedIds.length.toString()});
 		} else {
-			for (var i = 0;i < tabs.length;i++) {
-				for (var j = 0;j < PinnedIds.length;j++) {
-					if (tabs[i].id == PinnedIds[j]) {
-						chrome.tabs.update(tabs[i].id, {pinned: false});
-					}
-				}
-			}
-			for (var i = 0;i < tabs.length;i++) {
-				for (var j = 0;j < PinnedIds.length;j++) {
-					if (tabs[i].id == PinnedIds[j]) {
-						chrome.tabs.move(tabs[i].id, {index: Indexes[tabs[i].id]});
-						delete Indexes[PinnedIds[j]];
-						delete PinnedIds[j];
-					}
-				}
-			}
+			UnpinTabs(tabs);
+			ReorderTabs(tabs);
 			PinnedIds.length = 0;
 			chrome.browserAction.setBadgeText({text: ""});
 		}
