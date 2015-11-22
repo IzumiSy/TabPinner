6356b39617112f9486be692ab99738bc7f48c63a
IzumiSy <beetle-noise@gmx.com>
2015-11-22 12:53:45 +0900

    Create directories and fix indentations

---
+++ js/options.js
@@ -0,0 +1,17 @@
+
+// options.js
+
+document.getElementById('save').onclick = function() {
+  localStorage.EmergencyBtnIgnoreQueries = document.getElementById('ignores').value;
+  localStorage.EmergencyBtnTargetize = document.getElementById('targetize').checked;
+  window.close();
+};
+
+document.body.onload = function() {
+  var currentQueries = localStorage.EmergencyBtnIgnoreQueries;
+  var targetize = localStorage.EmergencyBtnTargetize;
+  if (currentQueries) {
+    document.getElementById('ignores').value = currentQueries;
+  }
+  document.getElementById("targetize").checked = localStorage.EmergencyBtnTargetize == "true" ? true : false;
+};
